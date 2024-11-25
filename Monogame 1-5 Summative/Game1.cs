bffaeafb11fb231a379be3c030081b0b8a6f7972
jcampeau25 <jdccanuck25@gmail.com>
2024-11-25 09:25:56 -0500

    Add project files.

---
+++ Monogame 1-5 Summative/Game1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_1_5_Summative
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window = new Rectangle(0, 0, 800, 500);
+
+        Texture2D introTexture, kanyeTexture, stageTexture;
+
+
+        Screen screen;
+
+        MouseState mouseState;
+
+        enum Screen
+        {
+            Intro,
+            Audience,
+            Stage
+        }
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight= 500;
+            screen = Screen.Intro;
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            introTexture = Content.Load<Texture2D>("vmaIntro");
+            stageTexture = Content.Load<Texture2D>("vmaStage");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            mouseState = Mouse.GetState();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                {
+                    screen = Screen.Stage;
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(introTexture, window, Color.White);
+            }
+
+            else if (screen == Screen.Stage)
+            {
+                _spriteBatch.Draw(stageTexture, window, Color.White);
+
+            }
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Monogame 1-5 Summative/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_1_5_Summative.Game1();
+game.Run();
